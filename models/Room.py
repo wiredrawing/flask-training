16de6d78bdf88a6da17b8cbcff2eb0517d69cd0d
Senbiki-Akifumi <a-senbiki@sunmark.bz>
2023-11-07 00:36:41 +0900

    メッセージの投稿ページをを作成

--- models/Room.py
+++ models/Room.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, Integer, Text
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 from lib.setting import session, base, engine
+from models.Message import Message
 
 
 class Room(base):
@@ -14,8 +15,8 @@ class Room(base):
     id = Column(Integer, primary_key=True, autoincrement=True, comment="ルームID")
     room_name = Column(Text(), nullable=False, comment="ルーム名")
     description = Column(Text(), nullable=False, comment="ルームの説明")
-
-    messages = relationship("Message", backref="room")
+    messages = relationship("Message", backref=backref("room"))
+    # messages = relationship("Message", backref="room", secondary=Message.__table__, order_by=Message.id.desc())
 
 
 if __name__ == "__main__":
--- routes/room.py
+++ routes/room.py
@@ -1,7 +1,8 @@
-from flask import Flask, Blueprint, render_template, redirect
+from flask import Flask, Blueprint, render_template, redirect, request
 from flask_login import current_user
 
 from lib.setting import session
+from models.Message import Message
 from models.Room import Room
 
 app = Blueprint('room', __name__, url_prefix='/room')
@@ -13,12 +14,49 @@ def index():
     # 要ログイン
     if current_user.is_authenticated is not True:
         return redirect("/login")
+    # 現在稼働中のチャットルーム一覧を表示
+    rooms = session.query(Room).all()
     """現在,作成されているチャットルーム一覧を表示"""
-    return render_template("room/index.html")
+    return render_template("room/index.html", rooms=rooms)
 
 
 @app.route("/<int:room_id>", methods=['GET'])
 def room(room_id):
+    if current_user.is_authenticated is not True:
+        return redirect("/login")
+    print(current_user)
+    print(current_user.get_id())
+    print("current_user.id ===> {}".format(current_user.id))
+    print(dir(current_user))
+    print("current_user.id ===> {}".format(current_user.id))
+
     room = session.query(Room).filter(Room.id == room_id).first()
+    print(dir(room))
     """指定されたチャットルームに紐づくメッセージを表示"""
-    return render_template("room/room.html", room=room)
+    return render_template("room/room.html", room=room, user=current_user, messages=room.messages)
+
+
+@app.route("/message/create/<int:room_id>", methods=['POST'])
+def create_message(room_id):
+    try:
+        required_keys = [
+            "user_id",
+            "message",
+        ]
+        post_data = request.form.to_dict()
+
+        # 正しいpostデータの場合はメッセージを登録
+        if required_keys == list(post_data.keys()):
+            message = Message()
+            message.message = post_data["message"]
+            message.room_id = room_id
+            message.user_id = post_data["user_id"]
+            session.add(message)
+            session.commit()
+            pass
+
+        return "";
+    except Exception as e:
+        print(e)
+
+    return ""
