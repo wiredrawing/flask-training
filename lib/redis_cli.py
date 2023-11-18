13074d25f8342b35a06c29416f740b521d7517b8
Senbiki-Akifumi <1400744+wiredrawing@users.noreply.github.com>
2023-11-18 23:39:03 +0900

    redis-serverの操作インターフェースを修正

--- lib/redis_cli.py
+++ lib/redis_cli.py
@@ -2,16 +2,31 @@ import redis
 import configparser
 
 
+class RedisCli:
+    """redis-serverへの接続を行う"""
+
+    def __init__(self, host: str, port: int, db: int):
+        self.redis = None
+        self.host = host
+        self.port = port
+        self.db = db
+
+    def connect(self):
+        self.redis = redis.Redis(host=self.host, port=self.port, db=self.db)
+        return self.redis
+
+
 def execute_redis():
     """Redisサーバーの起動を行い,接続したredis-cliを返却する"""
     try:
         config = configparser.ConfigParser()
         config.read('redis.ini')
-        redis_host = config["redis-server"]["host"]
-        redis_port = config["redis-server"]["port"]
-        # 一応typehinを付与
-        r: redis.Redis = redis.Redis(host=redis_host, port=redis_port, db=0)
-        r.ping()
+        rc = RedisCli(
+            host=config["redis-server"]["host"],
+            port=int(config["redis-server"]["port"]),
+            db=0)
+        r = rc.connect();
+        r.ping();
         return r
     except Exception as e:
         print("--> Redisサーバーの起動に失敗しました")
